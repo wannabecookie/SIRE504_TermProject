1cac214804a43f5ea72de6e10635c9bf57ad991d
wannabecookie <114206953+wannabecookie@users.noreply.github.com>
2025-11-19 14:40:49 +0700

    Update cli.py

--- fastq_pipeline/cli.py
+++ fastq_pipeline/cli.py
@@ -15,13 +15,11 @@ def argparser():
         description="Please choose command below:",
         dest='command',  
         required=True,
-        
     )
 
     fastq_pipeline = subparsers.add_parser('filteredProg', help='A utility that extracts barcodes from compressed FASTQ files and generates a filtered output file based on a statistically derived quality cutoff.')
     fastq_pipeline.add_argument('-i', '--input-file', type=str, default=None, required=True, help="Input fastq.gz file")
     fastq_pipeline.add_argument('-o', '--output-dir', type=str, default='../data/processed_data', required=False, help="Output directory")
-    
     
     barcode_extract = subparsers.add_parser('extractBQ', help='Extract barcode from fastq.gz to new file')
     barcode_extract.add_argument('-i', '--input-file', type=str, default=None, required=True, help="Input fastq.gz file")
@@ -36,5 +34,3 @@ def argparser():
     filter_file.add_argument('-b', '--barcode-dir', type=str, default='../data/processed_data', required=False, help="Input extracted barcode directory")
     
     return parser
-
-    
